e04969761dab3447a375600cf4b4158955f5a65e
Jason Koch <jasonk@bluedevel.com>
2021-11-29 22:25:58 -0800

    ensure charts respond to width of window (#2)

--- src/GexDixSpxScatter.js
+++ src/GexDixSpxScatter.js
@@ -1,4 +1,5 @@
 import { useState, useEffect, useRef } from 'react'
+import { useWindowDimensions } from './hooks'
 import * as d3 from 'd3'
 import PropTypes from 'prop-types'
 
@@ -13,24 +14,28 @@ function GexDixSpxScatter({data, recentData}) {
     const plotArea = useRef(null)
     const recentLineArea = useRef(null)
 
-    const height = 800
-    const width = 800
     const margin = {left: 100, right: 100, top: 100, bottom: 100}
+
+    const {width} = useWindowDimensions()
+    const chartWidth = Math.min((0.8 * width), 1200)
+    const chartHeight = chartWidth
 
     const renderArea = () => {
         const renderData = data
             .filter(d => d.dix >= DIX_MIN && d.dix <= DIX_MAX)
             .filter(d => d.gex >= GEX_MIN && d.gex <= GEX_MAX)
 
-        const xScale = d3.scaleLinear().domain([DIX_MIN, DIX_MAX]).range([0, width])
-        const yScale = d3.scaleLinear().domain([GEX_MIN, GEX_MAX]).range([height, 0])
+        const xScale = d3.scaleLinear().domain([DIX_MIN, DIX_MAX]).range([0, chartWidth])
+        const yScale = d3.scaleLinear().domain([GEX_MIN, GEX_MAX]).range([chartHeight, 0])
         const colorScale = d3.scaleLinear().domain([-0.03, 0, 0.06]).range([0, 0.5, 1])
         const recentColorScale = d3.scaleLinear().domain([0, recentData.length]).range([0, 1])
 
         const plotAreaG = d3.select(plotArea.current)
 
+        plotAreaG.selectAll('*').remove()
+
         plotAreaG.append('g')
-            .attr('transform', `translate(0,${height})`)
+            .attr('transform', `translate(0,${chartHeight})`)
             .call(d3.axisBottom(xScale).tickFormat(v => `${(v*100).toFixed(0)}%`))
 
         plotAreaG.append('g')
@@ -51,6 +56,8 @@ function GexDixSpxScatter({data, recentData}) {
 
         const recentLineAreaG = d3.select(recentLineArea.current)
 
+        recentLineAreaG.selectAll('*').remove()
+
         recentLineAreaG.append('path')
             .datum(recentData)
             .attr('opacity', 1)
@@ -64,10 +71,10 @@ function GexDixSpxScatter({data, recentData}) {
         if (plotArea.current && data.length > 0) {
             renderArea()
         }
-    }, [plotArea, data, recentData])
+    }, [plotArea, data, recentData, width])
 
     return (
-        <svg width={width+margin.left+margin.right} height={height+margin.top+margin.bottom}>
+        <svg width={chartWidth+margin.left+margin.right} height={chartHeight+margin.top+margin.bottom}>
             <g ref={plotArea} transform={`translate(${margin.left}, ${margin.top})`}/>
             <g ref={recentLineArea} transform={`translate(${margin.left}, ${margin.top})`}/>
         </svg>)
