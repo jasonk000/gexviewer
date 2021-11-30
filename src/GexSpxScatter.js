fb086f673958f05a84ee075fcfaaf38838a4482e
Jason Koch <jkoch@netflix.com>
2021-11-29 22:14:04 -0800

    ensure charts respond to width of window

--- src/GexSpxScatter.js
+++ src/GexSpxScatter.js
@@ -1,4 +1,5 @@
 import { useState, useEffect, useRef } from 'react'
+import { useWindowDimensions } from './hooks'
 import * as d3 from 'd3'
 import PropTypes from 'prop-types'
 
@@ -13,8 +14,11 @@ function GexSpxScatter({data, recentData}) {
     const plotArea = useRef(null)
     const verticalBars = useRef(null)
 
-    const height = 400
-    const width = 800
+    const {width} = useWindowDimensions()
+
+    const chartWidth = Math.min((0.8 * width), 1200)
+    const chartHeight = (chartWidth / 2)
+
     const margin = {left: 100, right: 100, top: 100, bottom: 100}
 
     const renderArea = () => {
@@ -22,15 +26,17 @@ function GexSpxScatter({data, recentData}) {
             .filter(d => d.gex >= GEX_MIN && d.gex <= GEX_MAX)
             .filter(d => d.forward >= FORWARD_MIN && d.forward <= FORWARD_MAX)
 
-        const xScale = d3.scaleLinear().domain([GEX_MIN, GEX_MAX]).range([0, width])
-        const yScale = d3.scaleLinear().domain([FORWARD_MIN, FORWARD_MAX]).range([height, 0])
+        const xScale = d3.scaleLinear().domain([GEX_MIN, GEX_MAX]).range([0, chartWidth])
+        const yScale = d3.scaleLinear().domain([FORWARD_MIN, FORWARD_MAX]).range([chartHeight, 0])
 
         const vertColorScale = d3.scaleLinear().domain([0, recentData.length]).range([0, 1])
 
         const plotAreaG = d3.select(plotArea.current)
 
+        plotAreaG.selectAll('*').remove()
+
         plotAreaG.append('g')
-            .attr('transform', `translate(0,${height})`)
+            .attr('transform', `translate(0,${chartHeight})`)
             .call(d3.axisBottom(xScale).tickFormat(v => `${(v/1000000000).toFixed(0)} bn`))
 
         plotAreaG.append('g')
@@ -50,6 +56,8 @@ function GexSpxScatter({data, recentData}) {
     
         const verticalBarsG = d3.select(verticalBars.current)
 
+	verticalBarsG.selectAll('*').remove()
+
         verticalBarsG.selectAll()
             .data(recentData)
             .enter()
@@ -57,17 +65,17 @@ function GexSpxScatter({data, recentData}) {
             .attr('opacity', 1)
             .attr('stroke', (d, i) => d3.interpolateBuGn(vertColorScale(i)))
             .attr('stroke-width', '1px')
-            .attr('d', d => `M${xScale(d.gex).toFixed()},${height} ${xScale(d.gex).toFixed()},0`)
+            .attr('d', d => `M${xScale(d.gex).toFixed()},${chartHeight} ${xScale(d.gex).toFixed()},0`)
     }
 
     useEffect(() => {
-        if (plotArea.current && data.length > 0) {
+        if (plotArea.current && data.length > 0 && width > 0) {
             renderArea()
         }
-    }, [plotArea, verticalBars, data])
+    }, [plotArea, verticalBars, data, width])
 
     return (
-        <svg width={width+margin.left+margin.right} height={height+margin.top+margin.bottom}>
+        <svg width={chartWidth+margin.left+margin.right} height={chartHeight+margin.top+margin.bottom}>
             <g ref={plotArea} transform={`translate(${margin.left}, ${margin.top})`}/>
             <g ref={verticalBars} transform={`translate(${margin.left}, ${margin.top})`}/>
         </svg>)
